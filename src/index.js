88e28dff613e27aa6e85dcf7ff8159bfd8f4c7ab
Thales Anjos <thales.anjos@bemind.com.br>
2021-09-11 21:26:19 -0300

    partial solution in corret time

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+
 export default class OrdersAnalyzer {
     constructor() {
         this.weekdays = ["SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"];
+        this.output = {};
     }
 
     averageDailySales(productId, orders) {
+        console.log(productId)
+        
+        let counter = {}
+        this.weekdays.forEach((weekday, index) => {
+            counter[index] = 0
+        })
+        
+        this.output[productId] = counter
 
-        // TODO: Implement
+        orders.forEach(order => {
+            let dayOfWeekNumber = new Date(order.creationDate).getDay()
+            let orderLines = order.orderLines
+            let ordersByDay = orderLines.filter(product => product.productId == productId)
+            if (ordersByDay.length > 0)
+                // console.log(ordersByDay[0])
+                this.output[productId][dayOfWeekNumber] += ordersByDay[0].quantity
+        });
 
+        console.log(this.output)
         return {};
     }
 }
