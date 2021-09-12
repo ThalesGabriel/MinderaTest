deefd9f25cab0946e5e4c041e96a32761d6d7eab
Thales Anjos <thales.anjos@bemind.com.br>
2021-09-12 00:15:36 -0300

    solution

--- src/index.js
+++ src/index.js
@@ -2,29 +2,38 @@
 export default class OrdersAnalyzer {
     constructor() {
         this.weekdays = ["SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"];
-        this.output = {};
+        this.output = {}
+    }
+
+    buildOutputObject(productId) {
+        this.output[productId] = {}
+        this.weekdays.forEach(weekday => {
+            this.output[productId][weekday] = 0
+        })
     }
 
     averageDailySales(productId, orders) {
-        console.log(productId)
-        
-        let counter = {}
-        this.weekdays.forEach((weekday, index) => {
-            counter[index] = 0
-        })
-        
-        this.output[productId] = counter
+        const c = {}
+
+        this.buildOutputObject(productId)
 
         orders.forEach(order => {
             let dayOfWeekNumber = new Date(order.creationDate).getDay()
             let orderLines = order.orderLines
             let ordersByDay = orderLines.filter(product => product.productId == productId)
-            if (ordersByDay.length > 0)
-                // console.log(ordersByDay[0])
-                this.output[productId][dayOfWeekNumber] += ordersByDay[0].quantity
+            if (ordersByDay.length > 0) {
+                let preSet = c[dayOfWeekNumber] ? [...c[dayOfWeekNumber]] : []
+                c[dayOfWeekNumber] = [...ordersByDay, ...preSet]
+            }
         });
+        
+        const reducer = (previousValue, currentValue) => previousValue.quantity + currentValue.quantity;
 
-        console.log(this.output)
-        return {};
+        Object.keys(c).forEach(key => {
+            let averageDailySale = c[key] = c[key].length > 1? Math.ceil(c[key].reduce(reducer)/c[key].length) : c[key][0].quantity
+            this.output[productId][this.weekdays[key]] = averageDailySale
+        })
+
+        return this.output[productId];
     }
 }
